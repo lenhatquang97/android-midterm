de77b767bdc1b0c691b618ae6eff69f128b34ab9
nguyenkhoa <nguyenkhoa0721@gmail.com>
2021-11-22 09:30:25 +0700

    fix add debt screen

--- lib/screens/add_debt.dart
+++ lib/screens/add_debt.dart
@@ -39,193 +39,188 @@ class _AddDebtState extends State<AddDebt> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text(
-          "Debt Details",
-          style: TextStyle(color: Colors.white),
-        ),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text(
+            "Thêm khoản nợ",
+            style: TextStyle(color: Colors.white),
           ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
         ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: RichText(
-                  text: TextSpan(
-                      style: const TextStyle(color: Colors.black, fontSize: 16),
-                      children: [
-                        const TextSpan(
-                            text: "Khoan ghi ngay ",
-                            style: TextStyle(fontSize: 20)),
-                        TextSpan(
-                          text: _date_time.day.toString() +
-                              "/" +
-                              _date_time.month.toString() +
-                              "/" +
-                              _date_time.year.toString(),
-                          style: const TextStyle(
-                              color: Colors.blue,
-                              fontSize: 20,
-                              decoration: TextDecoration.underline),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              showDatePicker(
-                                      context: context,
-                                      initialDate: DateTime.now(),
-                                      firstDate: DateTime(2001),
-                                      lastDate: DateTime(2222))
-                                  .then((date) => {_set_date_time(date!)});
-                            },
-                        )
-                      ]),
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Column(children: [
+                      Text("KHOẢN GHI NGÀY", style: TextStyle(fontSize: 30)),
+                      RichText(
+                        text: TextSpan(
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 16),
+                            children: [
+                              TextSpan(
+                                text: _date_time.day.toString() +
+                                    "/" +
+                                    _date_time.month.toString() +
+                                    "/" +
+                                    _date_time.year.toString(),
+                                style: const TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 20,
+                                    decoration: TextDecoration.underline),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    showDatePicker(
+                                            context: context,
+                                            initialDate: DateTime.now(),
+                                            firstDate: DateTime(2001),
+                                            lastDate: DateTime(2222))
+                                        .then(
+                                            (date) => {_set_date_time(date!)});
+                                  },
+                              )
+                            ]),
+                      ),
+                    ])),
+              ),
+              const SizedBox(height: 30),
+              Center(
+                child: Container(
+                  height: 60,
+                  margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 5),
+                      Expanded(
+                        child: ConstrainedBox(
+                          constraints:
+                              const BoxConstraints.tightFor(height: 50),
+                          child: ElevatedButton(
+                            onPressed: _left_button_click,
+                            child: const Text(
+                              'Tôi cho nợ',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            style: ButtonStyle(
+                                shadowColor: MaterialStateProperty.all(
+                                    Colors.transparent),
+                                backgroundColor: _button_index
+                                    ? MaterialStateProperty.all(
+                                        Colors.green[400])
+                                    : MaterialStateProperty.all(Colors.grey),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ))),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      Expanded(
+                        flex: 1,
+                        child: ConstrainedBox(
+                          constraints:
+                              const BoxConstraints.tightFor(height: 50),
+                          child: ElevatedButton(
+                            onPressed: _right_button_click,
+                            child: const Text(
+                              "Tôi nợ",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            style: ButtonStyle(
+                                shadowColor: MaterialStateProperty.all(
+                                    Colors.transparent),
+                                backgroundColor: !_button_index
+                                    ? MaterialStateProperty.all(Colors.red[400])
+                                    : MaterialStateProperty.all(Colors.grey),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ))),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 5)
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ),
-          const SizedBox(height: 30),
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: Container(
-                height: 60,
-                margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Row(
+              const SizedBox(height: 30),
+              Expanded(
+                child: Column(
                   children: [
-                    const SizedBox(width: 5),
-                    Expanded(
-                      flex: 1,
-                      child: ConstrainedBox(
-                        constraints: const BoxConstraints.tightFor(height: 50),
-                        child: ElevatedButton(
-                          onPressed: _left_button_click,
-                          child: const Text(
-                            'Toi cho no',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          style: ButtonStyle(
-                              shadowColor:
-                                  MaterialStateProperty.all(Colors.transparent),
-                              backgroundColor: _button_index
-                                  ? MaterialStateProperty.all(Colors.green[400])
-                                  : MaterialStateProperty.all(Colors.grey),
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5),
-                              ))),
-                        ),
-                      ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      textAlignVertical: const TextAlignVertical(y: 0.5),
+                      decoration: InputDecoration(
+                          hintText:
+                              _button_index ? "Tên người nợ" : "Tên chủ nợ",
+                          prefixIcon: const Icon(Icons.person)),
                     ),
-                    const SizedBox(width: 5),
-                    Expanded(
-                      flex: 1,
-                      child: ConstrainedBox(
-                        constraints: const BoxConstraints.tightFor(height: 50),
-                        child: ElevatedButton(
-                          onPressed: _right_button_click,
-                          child: const Text(
-                            "Toi no",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          style: ButtonStyle(
-                              shadowColor:
-                                  MaterialStateProperty.all(Colors.transparent),
-                              backgroundColor: !_button_index
-                                  ? MaterialStateProperty.all(Colors.red[400])
-                                  : MaterialStateProperty.all(Colors.grey),
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5),
-                              ))),
-                        ),
-                      ),
+                    TextFormField(
+                      textAlignVertical: const TextAlignVertical(y: 0.5),
+                      decoration: const InputDecoration(
+                          hintText: 'Số tiền',
+                          prefixIcon: Icon(Icons.attach_money)),
                     ),
-                    const SizedBox(width: 5)
+                    TextFormField(
+                      textAlignVertical: const TextAlignVertical(y: 0.5),
+                      decoration: const InputDecoration(
+                          hintText: 'Mô tả',
+                          prefixIcon: Icon(Icons.description)),
+                    ),
+                    TextFormField(
+                      textAlignVertical: const TextAlignVertical(y: 0.5),
+                      decoration: const InputDecoration(
+                          hintText: 'Số điện thoại',
+                          prefixIcon: Icon(Icons.phone)),
+                    ),
                   ],
                 ),
               ),
-            ),
+              Row(
+                children: [
+                  const SizedBox(width: 10),
+                  Expanded(
+                    flex: 1,
+                    child: ConstrainedBox(
+                        constraints: const BoxConstraints.tightFor(height: 50),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Luu"),
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(15)))),
+                        )),
+                  ),
+                  const SizedBox(width: 10),
+                ],
+              ),
+            ],
           ),
-          const SizedBox(height: 30),
-          Expanded(
-            flex: 5,
-            child: Column(
-              children: [
-                const Divider(
-                  thickness: 1,
-                  color: Colors.grey,
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                  textAlignVertical: const TextAlignVertical(y: 0.5),
-                  decoration: InputDecoration(
-                      hintText: _button_index ? "Ten nguoi no" : "Ten chu no",
-                      prefixIcon: const Icon(Icons.person)),
-                ),
-                TextFormField(
-                  textAlignVertical: const TextAlignVertical(y: 0.5),
-                  decoration: const InputDecoration(
-                      hintText: 'So tien',
-                      prefixIcon: Icon(Icons.attach_money)),
-                ),
-                TextFormField(
-                  textAlignVertical: const TextAlignVertical(y: 0.5),
-                  decoration: const InputDecoration(
-                      hintText: 'Mo ta', prefixIcon: Icon(Icons.description)),
-                ),
-                TextFormField(
-                  textAlignVertical: const TextAlignVertical(y: 0.5),
-                  decoration: const InputDecoration(
-                      hintText: 'So dien thoai', prefixIcon: Icon(Icons.phone)),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: [
-                const SizedBox(width: 10),
-                Expanded(
-                  flex: 1,
-                  child: ConstrainedBox(
-                      constraints: const BoxConstraints.tightFor(height: 50),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text("Luu"),
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15)))),
-                      )),
-                ),
-                const SizedBox(width: 10),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
